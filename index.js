d252df199467efda672870451823679e70bdf204
Jack Loflin <loflinj@gmail.com>
2025-12-03 19:06:13 -0800

    Added group joining logic on server side and ensured consistent data types for group identification.

--- index.js
+++ index.js
@@ -29,26 +29,43 @@ socketIO.on("connection", (socket) => {
   });
 
   socket.on("createNewGroup", (currentGroupName) => {
-    console.log(currentGroupName);
-    chatgroups.unshift({
+    console.log(`Creating and Joining group: ${currentGroupName}`);
+    const newGroup = {
       id: chatgroups.length + 1,
       currentGroupName,
       messages: [],
-    });
+    };
+    chatgroups.unshift(newGroup);
+
+    // FIX 1: Make the creating user join the specific Socket.IO room
+    socket.join(currentGroupName); 
+
     socket.emit("groupList", chatgroups);
   });
 
   socket.on("findGroup", (id) => {
-    const filteredGroup = chatgroups.filter((item) => item.id === id);
-    socket.emit("foundGroup", filteredGroup[0].messages);
-    console.log("found group");
+    // Note: 'id' is a number, groupIdentifier is a number, currentGroupName is a string
+    const filteredGroups = chatgroups.filter((item) => item.id === id);
+    if (filteredGroups.length > 0) {
+        const group = filteredGroups[0];
+        // FIX 2: Make the joining user join the specific Socket.IO room
+        socket.join(group.currentGroupName); 
+        socket.emit("foundGroup", group.messages);
+        console.log(`Socket ${socket.id} joined room: ${group.currentGroupName}`);
+    }
   });
 
   socket.on("newChatMessage", (data) => {
     const { currentChatMesage, groupIdentifier, currentUser, timeData } = data;
-    const filteredGroup = chatgroups.filter(
+    const filteredGroups = chatgroups.filter(
       (item) => item.id === groupIdentifier
     );
+    
+    if (filteredGroups.length === 0) return; // Exit if group not found
+    
+    const group = filteredGroups[0];
+    const roomName = group.currentGroupName; // Get the string name of the room
+
     const newMessage = {
       id: createUniqueId(),
       text: currentChatMesage,
@@ -56,12 +73,17 @@ socketIO.on("connection", (socket) => {
       time: `${timeData.hr}:${timeData.mins}`,
     };
 
-    socket
-      .to(filteredGroup[0].currentGroupName)
-      .emit("groupMessage", newMessage);
-    filteredGroup[0].messages.push(newMessage);
-    socket.emit("groupList", chatgroups);
-    socket.emit("foundGroup", filteredGroup[0].messages);
+    // *** FIX 3: Broadcast to everyone in the room using the main 'socketIO' instance ***
+    // This sends the message to *all* sockets that have called socket.join(roomName),
+    // including the sender itself.
+    socketIO.to(roomName).emit("groupMessage", newMessage);
+    
+    // Update the server's state
+    group.messages.push(newMessage);
+    
+    // The following two lines might be redundant if your clients rely only on the 'groupMessage' listener:
+    // socket.emit("groupList", chatgroups);
+    // socket.emit("foundGroup", filteredGroup[0].messages);
   });
 });
 
